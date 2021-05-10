33d400a111ac6a88ec80258b6a72f9d856685c25
ridhamgoyal07 <ridhamgoyal07@gmail.com>
2021-05-10 19:08:27 +0530

    loading files

---
+++ server.js
@@ -0,0 +1,34 @@
+// getting access to express module 
+const express = require("express");
+const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const dotenv = require("dotenv");
+const path = require("path");
+
+
+const app = express();
+
+dotenv.config({path:'./config.env'});
+const PORT = process.env.PORT || 8080 ;
+
+// log requests 
+app.use(morgan("tiny"));
+
+// parse request to body-parser module 
+app.use(bodyparser.urlencoded({extended:true}));
+
+// set view engine
+app.set("view engine" , "ejs");
+// app.set("views",path.resolve(__dirname, "views/ejs"));
+
+// load assets
+app.use('/css', express.static(path.resolve(__dirname , "assets/css")));
+ 
+app.get('/',(req,res)=>{
+    res.send("CRUD Application is Running");
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
